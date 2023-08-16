6ddf81f1fe84ade74722f97fb78ad7f0f1e2fda2
whygizzmo <mrcool200275@gmail.com>
2023-08-16 14:31:23 +0600

    createProj

---
+++ src/main/java/kg/mega/remoteTgBot/RemoteTgBotApplication.java
@@ -0,0 +1,13 @@
+package kg.mega.remoteTgBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RemoteTgBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RemoteTgBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kg/mega/remoteTgBot/RemoteTgBotApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.mega.remoteTgBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RemoteTgBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
