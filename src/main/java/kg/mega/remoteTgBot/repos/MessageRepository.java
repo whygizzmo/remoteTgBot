ca52fb46faa3dba40d982a6d9f58c7f538768d07
whygizzmo <mrcool200275@gmail.com>
2023-08-17 16:26:05 +0600

    method repo findAllByEmployee change to nativeQuery

--- src/main/java/kg/mega/remoteTgBot/repos/MessageRepository.java
+++ src/main/java/kg/mega/remoteTgBot/repos/MessageRepository.java
@@ -2,11 +2,13 @@ package kg.mega.remoteTgBot.repos;
 
 import kg.mega.remoteTgBot.models.entities.Message;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface MessageRepository extends JpaRepository<Message,Long> {
+    @Query(value = "SELECT * FROM message m WHERE m.employee_id = ?1 AND DATE_PART('day', NOW() - m.date) < 31", nativeQuery = true)
     List<Message> findAllByEmployeeId(long employeeId);
 }
